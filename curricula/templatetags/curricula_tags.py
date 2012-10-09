758dbb5c9d5e9a9a9e254a30d52c30071cdd07e4
Rajit Sarkar <rsarkar@celerity.com>
2012-10-09 10:26:31 -0400

    [EDU-2943] GenericArticle model does not support get_related_content_type method (yet)

--- curricula/templatetags/curricula_tags.py
+++ curricula/templatetags/curricula_tags.py
@@ -89,7 +89,10 @@ class RelatedNode(template.Node):
         try:
             the_obj = self.object.resolve(context)
             if self.content_type:
-                context[self.var_name] = the_obj.get_related_content_type(self.content_type)
+                try:
+                    context[self.var_name] = the_obj.get_related_content_type(self.content_type)
+                except AttributeError:
+                    pass # fail quietly, for now
             elif self.relation_type:
                 context[self.var_name] = the_obj.get_relation_type(self.relation_type)
             else:
